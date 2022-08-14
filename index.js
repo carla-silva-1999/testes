4f820a8152027c7d3aaa74e0be2112ae518ae3a5
carla-moreira-1999 <karllasillva85@gmil.com>
2022-08-14 16:14:15 -0300

    Subindo o repositorio

---
+++ index.js
@@ -0,0 +1,20 @@
+function selecionarOperacao(){
+    var n1 = document.getElementById("number1").value;
+    var n2 = document.getElementById('number2').value;
+     var res = document.getElementById('res');
+    var resulEnd = document.getElementById('operacoes').value;
+debugger
+    if(resulEnd == "somar +"){
+        res.value = "Resultado: " + (Number(n1) + Number(n2));
+    }
+    if(resulEnd == "subtrair -"){
+        res.value = 'Resultado: ' + (Number(n1) - Number(n2));
+    }
+    if(resulEnd == "multiplicar x"){
+        res.value = 'Resultado: ' + (Number(n1) * Number(n2));
+    }
+     if(resulEnd == "divisão /"){
+        res.value = 'Resultado: ' + (Number(n1) / Number(n2));
+    
+    }
+};
